030a039babefe66d73173d19cac9234161155b11
Maksim Kostromin <daggerok@gmail.com>
2019-06-11 02:01:54 +0300

    update-latest-scala-version | Fixed Bug :) (C) whatthecommit.com

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -3,15 +3,17 @@ import org.gradle.api.JavaVersion
 object Globals {
 
   object Project {
-    const val version = "1.1-SNAPSHOT"
+    const val version = "1.2-SNAPSHOT"
     const val artifactId = "functional-and-reactive-principles-in-scala"
     const val groupId = "com.github.daggerok"
   }
 
   val javaVersion = JavaVersion.VERSION_1_8
   //const val scalaMinorVersion = "3"
-  const val scalaBaselineVersion = "2.12"
-  const val scalaMinorVersion = "6"
+  const val scalaMinorVersion = "0"
+  const val scalaBaselineVersion = "2.13"
+  //const val scalaMinorVersion = "6"
+  //const val scalaBaselineVersion = "2.12"
   const val scalaVersion = "$scalaBaselineVersion.$scalaMinorVersion"
 
   const val junitVersion = "4.13-beta-3"
